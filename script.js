8c9728ac7256c1bcd0a81225623e5f039618360f
Monique Loibner <m.loibner@hotmail.com>
2020-06-06 18:32:32 +1000

    renaming files and adding content from gitlab

---
+++ script.js
@@ -0,0 +1,16 @@
+const password = document.getSelection(".password")
+const button = document.getSelection(".button")
+
+// Assignment Code
+let generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+  let password = generatePassword();
+  let passwordText = document.querySelector('#password');
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
